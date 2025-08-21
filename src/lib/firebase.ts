661f1a1ef5e2c3165d68abdea5ff67c81dd1ea9a
Shreyan S <shreyanofficial25@gmail.com>
2025-08-21 05:03:13 +0000

    This is my Firebase SDK
    // Import the functions you need from the SDKs

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -7,13 +7,13 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID",
-  measurementId: "YOUR_MEASUREMENT_ID"
+  apiKey: "AIzaSyDUyxrtUDrIdWD0WeZAHCf3ye_rS7RxZLY",
+  authDomain: "deepsafe-hack.firebaseapp.com",
+  projectId: "deepsafe-hack",
+  storageBucket: "deepsafe-hack.appspot.com",
+  messagingSenderId: "200519422683",
+  appId: "1:200519422683:web:260440dabe47328ca4b2fe",
+  measurementId: "G-T06BF2FRR5"
 };
 
 // Initialize Firebase
